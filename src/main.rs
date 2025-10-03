88dc1ce6256c060ad5780e2dabbbfa43fde6207e
mateusz_baran <mbaran1819@gmail.com>
2025-10-03 11:32:29 +0200

    FUNCTIONALITY: added main functionality
    - reading the all posts from jsonplaceholder
    - creating json files where the names came from post id

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Post {
-    userId: i32,
+    userId: i32, //warning in the compilation - name came from jsonplacecholder
     id: i32,
     title: String,
     body: String,
@@ -12,10 +12,24 @@ struct Post {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let Post: Vec<Post> = reqwest::get("https://jsonplaceholder.typicode.com/posts")
+    let post_val: Vec<Post> = reqwest::get("https://jsonplaceholder.typicode.com/posts")
         .await?
         .json()
         .await?;
-    println!("Pobrano {} postow", Post.len());
+    let _ = save_post(post_val);
+    //println!("Pobrano {} postow", Post_val.len());
+
+    Ok(())
+}
+
+fn save_post(tab: Vec<Post>) -> Result<(), std::io::Error> {
+    let dir = "JSON_files";
+
+    for post in &tab {
+        let json = serde_json::to_string_pretty(post).unwrap();
+        let file_name = format!("{}/post_{}.json", dir, post.id);
+        let mut file = File::create(file_name)?;
+        file.write_all(json.as_bytes())?;
+    }
     Ok(())
 }
